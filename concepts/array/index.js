51b3da9234ada63209fb818edd58391c212dbc79
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-16 16:40:19 +0600

    array methods added

---
+++ concepts/array/index.js
@@ -0,0 +1,9 @@
+const numbers = [10, 25, 40, 55];
+const users = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+];
+const words = ['hello', 'world'];
+
+
+console.log(numbers.findIndex((num) => num < 40)); 
